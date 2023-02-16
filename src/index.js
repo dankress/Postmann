b3774edad01409786dcee3ba5307b1f79a89a813
JanSpecovius <JanSpecovius@users.noreply.github.com>
2023-02-16 21:44:31 +0100

    Added Packagestation
    Note: Not jet working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
-import router from "./shipmentsRoutes.js"
+import router from "./shipmentsRoutes.js";
+import router from "./packagestationsRoutes.js";
 
 
 const app = express();
@@ -8,6 +9,7 @@ const port = 4000;
 
 app.use(bodyParser.json());
 app.use("/shipments", router);
+app.use("/packagestation", router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
