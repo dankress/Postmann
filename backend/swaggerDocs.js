24151caad08c5c3f473cd0c74fb06728a72d3d70
vingre <116197033+vingre@users.noreply.github.com>
2023-02-27 11:24:44 +0100

    Update WiredTiger.lock

--- backend/swaggerDocs.js
+++ backend/swaggerDocs.js
@@ -9,5 +9,98 @@ const swaggerDocs={
     },
     host: "localhost:4000",
     basePath: "/",
-
+    tags: [
+        {
+          name: "Packagestations",
+          description: "packagestation in the database",
+        },
+      ],
+      consumes: ["application/json"],
+    produces: ["application/json"],
+    paths: {
+        "/packagestations": {
+          get: {
+            tags: ["Packagesations"],
+            summary: "Get all packagestations in the system",
+            responses: {
+                200: {
+                    description: "OK",
+                    schema: {
+                        $ref: "#/definitions/Package",
+                        },
+                    },
+                },
+            },
+            post: {
+                tags: ["Packagestations"],
+                summary: "Add a new packagestations",
+                parameters: [
+                    {
+                        name: "packagestation",
+                        in: "body",
+                        description: "Packagestation to be added",
+                        schema: {
+                            $ref: "#/definitions/Packagestation",
+                        },
+                    },
+                ],
+                responses: {
+                    201: {
+                        description: "Created",
+                        schema: {
+                            $ref: "#/definitions/Package",
+                        },
+                    },
+                },
+            },
+            "/{id}": {
+                get: {
+                    tags: ["Packagestations"],
+                    summary: "Get a specific packagestations by id",
+                    parameters: [
+                        {
+                            name: "id",
+                            in: "body",
+                            description: "Id of the packagestations searched for",
+                            schema: {
+                            $ref: "#/definitions/Packagestation",
+                            },
+                        },
+                    ],
+                    responses: {
+                        200: {
+                            description: "OK",
+                            schema: {
+                            $ref: "#/definitions/Package",
+                            },
+                        },
+                    },
+                },
+            },
+            "/search": {
+                get: {
+                    tags: ["Packagestations"],
+                    summary: "Get a specific packagestation by title",
+                    parameters: [
+                        {
+                            name: "title",
+                            in: "path",
+                            description: "Title of the packagestation searched for",
+                            schema: {
+                            $ref: "#/definitions/Packagestation",
+                            },
+                        },
+                    ],
+                    responses: {
+                        200: {
+                            description: "OK",
+                            schema: {
+                                $ref: "#/definitions/Packagestation",
+                            },
+                        },
+                    },
+                },
+            },
+    	}
+    }
 }
